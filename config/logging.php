635556936a093233525ad540560ca2603e497b0b
AG <fdwnct@gmail.com>
2022-08-13 16:48:00 +0430

    larabug exception tracker

--- config/logging.php
+++ config/logging.php
@@ -117,6 +117,10 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'larabug' => [
+            'driver' => 'larabug',
+        ],
     ],
 
 ];
